aac075c1abcd9bb3c6b5d8f9e109257772ae0ccb
Zarak Ansari <zarak.ansari@yahoo.com>
2024-05-19 19:38:48 +0500

    minimum viable app

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import io
-from flask import Flask, flash, request, redirect, send_file
+from flask import Flask, flash, request, redirect, send_file, render_template
 from easyocr import Reader
 from utils import create_excel_file, process_image
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
-
+app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024
 reader = Reader(['en'])
 
 def allowed_file(filename):
@@ -18,25 +18,21 @@ def allowed_file(filename):
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
+        left = int(request.form.get('left'))
+        top = int(request.form.get('top'))
+        right = int(request.form.get('right'))
+        bottom = int(request.form.get('bottom'))
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         results = []
         for file in request.files.getlist('file'):
             if file and allowed_file(file.filename):                
-                results.append(process_image(reader, file, 1550, 300, 1910, 380))
+                results.append(process_image(reader, file, left, top, right, bottom))
         
         excel_bytes_array = create_excel_file(results)
 
         return send_file(excel_bytes_array,
                         mimetype='application/vnd.ms-excel',
                         download_name='output.xlsx')
-    return '''
-    <!doctype html>
-    <title>Prize Bonds OCR</title>
-    <h1>Upload Scanned Images</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file multiple=true>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template('index.html')
